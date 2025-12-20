97c2de9c0af49a100c43074a256fc858bc39e1a4
I-HSUAN\beaut <beauty1997724@gmail.com>
2025-12-20 15:53:20 +0800

    加入專案檔案。

---
+++ ParallelProcessing/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoMapper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
